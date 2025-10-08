bd262d63bfa03e43a7465ac12763280cfc570f07
Mihálka Gábor (Gabi) <mihalkagabor@gmail.com>
2025-10-08 15:19:42 +0200

    Első inditás sikeres. Pom.xml, compose.yaml. application.yaml működik .

---
+++ src/main/java/org/mihalka/kapcsolatok_backend/KapcsolatokBackendApplication.java
@@ -0,0 +1,13 @@
+package org.mihalka.kapcsolatok_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapcsolatokBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapcsolatokBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mihalka/kapcsolatok_backend/KapcsolatokBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mihalka.kapcsolatok_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapcsolatokBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
